37448f9077327f5ed3d2a2d42ad2e9b78a248e0c
lumod97 <110913947+lumod97@users.noreply.github.com>
2023-01-19 23:02:31 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://lumod97.github.io/facturacion_electronica
+git push -f https://github.com/lumod97/facturacion_electronica.git master:gh-pages
+
+cd -
